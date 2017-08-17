b70120884ee959588a1aaf8a14922009672d6f68
Carlos Gonzalez <carlosegonzax@gmail.com>
2017-08-17 15:03:09 -0400

    init commit

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php 
+
+require "vendor/larapack/dd/src/helper.php";
+require "vendor/autoload.php";
+
+use Scheb\YahooFinanceApi\ApiClient;
+use Scheb\YahooFinanceApi\ApiClientFactory;
+use GuzzleHttp\Client;
+
+$client = ApiClientFactory::createApiClient();
+
+// Returns Scheb\YahooFinanceApi\Results\ExchangeRate
+$exchangeRate = $client->getExchangeRate("EUR", "USD");
+
+// Returns Scheb\YahooFinanceApi\Results\Quote
+$quote = $client->getQuote("AAPL");
+
+// Returns an array of Scheb\YahooFinanceApi\Results\ExchangeRate
+$exchangeRates = $client->getExchangeRates([
+    ["USD", "EUR"],
+    ["EUR", "USD"],
+]);
+
+dd($exchangeRates);
+
+?>
